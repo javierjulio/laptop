38aecb53302f93a0680ad887990abc9b572270d7
Javier Julio <jjfutbol@gmail.com>
2014-09-08 18:36:51 -0400

    Update test dir path for files generated by tests

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -5,11 +5,11 @@ FIXTURES_ROOT="${BATS_TEST_DIRNAME}/fixtures"
 
 export PATH="${SCRIPT_ROOT}:${FIXTURES_ROOT}:$PATH"
 
-LAPTOP_TEST_DIR="${BATS_TMPDIR}/laptop"
+LAPTOP_TEST_DIR="${BATS_TEST_DIRNAME}/tmp"
 
 dump() {
   rm -f "${SCRIPT_ROOT}/log.txt"
-  echo "$1" >> "${SCRIPT_ROOT}/log.txt"
+  echo "$@" >> "${SCRIPT_ROOT}/log.txt"
 }
 
 teardown() {
