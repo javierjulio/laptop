4f1eed2d6d6a5f6c256844557aa719d43f7cc7d3
Javier Julio <jjfutbol@gmail.com>
2020-03-15 02:20:18 -0400

    Remove test path filter
    This was because we had a test.sh file in the test directory which was just a scratchpad for experiments. Since we wanted that ignored, we needed that here but we don't have any sh files there anymore.

--- shellcheck.sh
+++ shellcheck.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 (
 # find all executables and run `shellcheck`
-for f in $(find . -type f -not -iwholename '*.git*' -not -path './test/*' | sort -u); do
+for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		shellcheck -x "$f" && echo "[OK]: sucessfully linted $f"
 	fi
