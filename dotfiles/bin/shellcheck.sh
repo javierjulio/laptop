536168ccd30bc6a370cd3a352fd0e9749ff5dd84
Javier Julio <jjfutbol@gmail.com>
2020-03-15 02:40:17 -0400

    Track status so exit is performed after full run
    We don't want to exit at the first failure but instead run through all files and if we encountered any error, then we'll exit with that status.

--- dotfiles/bin/shellcheck.sh
+++ dotfiles/bin/shellcheck.sh
@@ -3,11 +3,14 @@
 set -e
 set -o pipefail
 
-(
 # find all executables and run `shellcheck`
+
+(
+status=0
 for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
 	if file "$f" | grep --quiet shell; then
-		shellcheck -x "$f" && echo "[OK]: sucessfully linted $f" || exit 1
+		shellcheck -x "$f" && echo "[OK]: sucessfully linted $f" || status=1
 	fi
 done
+exit "$status"
 )
