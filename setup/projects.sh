6db2eb1cf3a14b818cfd4bdd862e1b157554c36a
Javier Julio <jjfutbol@gmail.com>
2023-02-11 11:57:18 -0500

    Support cloning by repo orgs
    I switched to bash due to shellcheck warnings with `local` usage. This works same as before but now can install repos by org and repo name.

--- setup/projects.sh
+++ setup/projects.sh
@@ -1,28 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 PROJECTS_PREFIX="$HOME/Projects"
 
 clone_repo() {
-  if [ ! -d "$PROJECTS_PREFIX/${2:-$1}" ]; then
-    git clone "https://github.com/javierjulio/$1.git" "$PROJECTS_PREFIX/${2:-$1}"
+  local dir
+  dir=$(basename "${2:-$1}")
+  if [ ! -d "$PROJECTS_PREFIX/$dir" ]; then
+    git clone "https://github.com/$1.git" "$PROJECTS_PREFIX/${dir}"
     echo "Cloned $1 successfully."
   fi
 }
 
-clone_repo "javierjulio.github.io" "javierjulio"
-clone_repo "activeadmin"
-clone_repo "arbre"
-clone_repo "binlist-elixir"
-clone_repo "binlist-node"
-clone_repo "cheats"
-clone_repo "chris-courses"
-clone_repo "game-dev"
-clone_repo "gdevelop-examples"
-clone_repo "how-to-program-games"
-clone_repo "humble-bundle-key-exporter"
-clone_repo "inherited_resources"
-clone_repo "manager-game-sources"
-clone_repo "memory-game"
-clone_repo "new-star-manager-data"
-clone_repo "nonograms-game"
-clone_repo "soccer-manager-game"
+clone_repo "javierjulio/javierjulio.github.io" "javierjulio"
+clone_repo "javierjulio/binlist-elixir"
+clone_repo "javierjulio/binlist-node"
+clone_repo "javierjulio/cheats"
+clone_repo "javierjulio/chris-courses"
+clone_repo "javierjulio/game-dev"
+clone_repo "javierjulio/gdevelop-examples"
+clone_repo "javierjulio/how-to-program-games"
+clone_repo "javierjulio/humble-bundle-key-exporter"
+clone_repo "javierjulio/manager-game-sources"
+clone_repo "javierjulio/memory-game"
+clone_repo "javierjulio/new-star-manager-data"
+clone_repo "javierjulio/nonograms-game"
+clone_repo "javierjulio/soccer-manager-game"
+
+clone_repo "activeadmin/activeadmin"
+clone_repo "activeadmin/arbre"
+clone_repo "activeadmin/inherited_resources"
